d20b63d8ee42bf653fd68b8c089fc35a529b6633
NickChuCode <nickchu15@outlook.com>
2019-01-03 22:17:34 +0800

    add reduceByKey in basicStat and its test

--- src/base/basicStat.js
+++ src/base/basicStat.js
@@ -11,3 +11,29 @@ export function meanAge (crew) {
 export function meanByAge (crew) {
   return _.meanBy(crew, 'age')
 }
+
+// 词频统计，一般来对数组中的各种可能性进行频次统计，是先创建一个用于记录频次的对象，
+// 然后通过遍历数组中的每一个元素，并将其一个一个放入到前面创建的对象中以记录频次。
+// 但是自从我们学会了使用 Map 和 Reduce 开始我们就可以使用更直观的方式进行统计。
+// 首先把每一个词使用变换函数将其变成一个以单词为第一元素，以 1 为第二元素的数组，
+// 我们可以将其称为 Tuple，相当于对象中的一个键值对。
+//   "hello" -> [ "hello", 1 ]
+// 既然我们将单词转换成了多个 Tuple 键值对的键，那么我们是不是可以使用这个特性更方便地进行 Reduce 呢？
+// 是的，我们可以将其称为 reduceByKey。在一般情况下的 Reduce 函数是用于遍历整个数组的，而 reduceByKey
+// 则是根据 Tuple 集中的键首先进行一次分类组合，将具有相同键的值进行组合，然后对每一个组合集进行单独遍历。
+// 不幸的是，无论是原生 JavaScript 中还是 Lodash 中并没有这样的 API。
+// 但是我们却可以使用 Lodash 的函数进行组合，对 Lodash 进行拓展。
+export function reduceByKey (tuples, reduceCallback) {
+  const grouped = _.groupBy(tuples, function (tuple) {
+    return tuple[0]
+  })
+
+  return _.toPairs(_.mapValues(grouped, function (tuples) {
+    return _.chain(tuples)
+      .map(function (tuple) {
+        return tuple[1]
+      })
+      .reduce(reduceCallback)
+      .value()
+  }))
+}
--- src/base/basicStat.test.js
+++ src/base/basicStat.test.js
@@ -1,4 +1,4 @@
-import { meanAge, meanByAge } from './basicStat'
+import { meanAge, meanByAge, reduceByKey } from './basicStat'
 import chai from 'chai'
 
 let expect = chai.expect
@@ -36,11 +36,31 @@ const crew = [
   }
 ]
 
+const testText = `
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+`
+const words = testText.toLowerCase().match(/\w+/g)
+const tuples = words.map(function (word) {
+  return [ word, 1 ]
+})
+
 describe('基本数据处理 - 基本统计测试', function () {
   it('均值测试', function () {
     expect(meanAge(crew)).to.be.equal(26.8)
   })
   it('meanBy均值测试', function () {
     expect(meanByAge(crew)).to.be.equal(26.8)
+  })
+  it('词频统计测试', function () {
+    expect(reduceByKey(tuples, function (left, right) {
+      return left + right
+    })).to.be.an('array')
+    expect(reduceByKey(tuples, function (left, right) {
+      return left + right
+    }).sort(function (left, right) {
+      return right[1] - left[1]
+    })[0][0]).to.be.equal('the')
   })
 })
